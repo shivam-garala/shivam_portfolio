157df52da35951b5e72c6887466344f94a73b817
Shivam Garala <shivam@phpdots.com>
2024-07-17 10:32:26 +0530

    Updates

---
+++ precache-manifest.d5cb8de3dc9494f2d91ac782058d31dc.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f4df9e94b04d9b2aa62227176ca5290",
+    "url": "/shivam_portfolio/index.html"
+  },
+  {
+    "revision": "2f3d37890f9dc38c6279",
+    "url": "/shivam_portfolio/static/css/main.c052b194.chunk.css"
+  },
+  {
+    "revision": "8e3b93f1ca2c45df4e30",
+    "url": "/shivam_portfolio/static/js/2.6060a0df.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/shivam_portfolio/static/js/2.6060a0df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f3d37890f9dc38c6279",
+    "url": "/shivam_portfolio/static/js/main.2556b90e.chunk.js"
+  },
+  {
+    "revision": "7d1f15a18d6f6a01b6ff",
+    "url": "/shivam_portfolio/static/js/runtime-main.7d2c6c08.js"
+  },
+  {
+    "revision": "80a80257157c3a6436157aca9b8a99f7",
+    "url": "/shivam_portfolio/static/media/Admin - Dashboard.80a80257.png"
+  },
+  {
+    "revision": "6ce24603f89ed54f3e4c49af521fbb4b",
+    "url": "/shivam_portfolio/static/media/Admin - Manage Users.6ce24603.png"
+  },
+  {
+    "revision": "e0251390d490033e2668d3b96f4bbf0f",
+    "url": "/shivam_portfolio/static/media/Admin - Manage Withdraw Requests.e0251390.png"
+  },
+  {
+    "revision": "66c71d6d14e0412d13aae57da7d237b8",
+    "url": "/shivam_portfolio/static/media/Dashboard (1).66c71d6d.png"
+  },
+  {
+    "revision": "020e178f38e1a30c62d8ea796ddc7a5b",
+    "url": "/shivam_portfolio/static/media/JEWEL_MAKER.020e178f.png"
+  },
+  {
+    "revision": "dc493c205c5d84ebe198a72a1074b581",
+    "url": "/shivam_portfolio/static/media/JEWEL_MAKERLogin.dc493c20.png"
+  },
+  {
+    "revision": "7b3ce55fb86f58fe049dad964c88fa3e",
+    "url": "/shivam_portfolio/static/media/Tuvoc-1.7b3ce55f.svg"
+  },
+  {
+    "revision": "d9ae3fc2c118621584504c3dfc6eb13c",
+    "url": "/shivam_portfolio/static/media/VKJ-GOLD_dashboard.d9ae3fc2.png"
+  },
+  {
+    "revision": "c492763ae34340b9aee90d37fdcbda2b",
+    "url": "/shivam_portfolio/static/media/VKJ-GOLDbannermanage.c492763a.png"
+  },
+  {
+    "revision": "d9883030e652a7ee94d7b272e863646c",
+    "url": "/shivam_portfolio/static/media/VKJGOLD_mobileappmanagement system.d9883030.png"
+  },
+  {
+    "revision": "3954ae83aee5d826f87380e5204d993e",
+    "url": "/shivam_portfolio/static/media/VKJ_GOLDproductmanaga.3954ae83.png"
+  },
+  {
+    "revision": "f0445c6c806e266800d6e9ed4462c271",
+    "url": "/shivam_portfolio/static/media/VKJ_GOLDtagging.f0445c6c.png"
+  },
+  {
+    "revision": "fdff4014b22f67787c176c6aa52890a7",
+    "url": "/shivam_portfolio/static/media/aws-2.fdff4014.svg"
+  },
+  {
+    "revision": "bf989778565c49da91f9e0ef13dbdc5c",
+    "url": "/shivam_portfolio/static/media/background.bf989778.webp"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/shivam_portfolio/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "9c517cee21e6bc67384619377ad5a7ea",
+    "url": "/shivam_portfolio/static/media/bitbucket-icon.9c517cee.svg"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/shivam_portfolio/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/shivam_portfolio/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "c6bab64b183f96aa47077fd3db37afd0",
+    "url": "/shivam_portfolio/static/media/express.c6bab64b.svg"
+  },
+  {
+    "revision": "faf8f499559f6b2f815f454a5794eae3",
+    "url": "/shivam_portfolio/static/media/flutter.faf8f499.svg"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "/shivam_portfolio/static/media/git-icon.1190a19c.svg"
+  },
+  {
+    "revision": "567f006b9ca974f343db1c9c79920074",
+    "url": "/shivam_portfolio/static/media/github.567f006b.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/shivam_portfolio/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/shivam_portfolio/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "2fa5a6b6d7ce810d2b67ebf6df87fcc8",
+    "url": "/shivam_portfolio/static/media/laravel-2.2fa5a6b6.svg"
+  },
+  {
+    "revision": "82291019f34ab54a677c456a84127cc3",
+    "url": "/shivam_portfolio/static/media/material-ui-1.82291019.svg"
+  },
+  {
+    "revision": "3ea10fb644c05e7c947611dae8d58a45",
+    "url": "/shivam_portfolio/static/media/mern_tip_calc.3ea10fb6.webp"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "/shivam_portfolio/static/media/mongodb.2985235d.svg"
+  },
+  {
+    "revision": "b423766d4d3b2da3942977c207206446",
+    "url": "/shivam_portfolio/static/media/mysql-logo.b423766d.svg"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/shivam_portfolio/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "d7c985e7cca077dc2d77f89a66573e35",
+    "url": "/shivam_portfolio/static/media/postgresql.d7c985e7.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/shivam_portfolio/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "8bce7aef886c3872c218e0e6ef8bdccb",
+    "url": "/shivam_portfolio/static/media/slide1.8bce7aef.webp"
+  },
+  {
+    "revision": "7256f309520276be70acd97720610bc6",
+    "url": "/shivam_portfolio/static/media/slide2.7256f309.webp"
+  },
+  {
+    "revision": "8e5b87b60151049a8df9ec0bd30978f0",
+    "url": "/shivam_portfolio/static/media/slide3.8e5b87b6.webp"
+  },
+  {
+    "revision": "15ff424255016808e1caa37ab0313bf8",
+    "url": "/shivam_portfolio/static/media/wepik-export-20240114120421W9dd.15ff4242.png"
+  }
+]);
